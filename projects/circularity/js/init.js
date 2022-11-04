c6b22fd97b74ca75e4761dd4ad31636f66903951
aden-berry <berry446@stcharles.k12.la.us>
2022-11-04 13:14:47 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle;
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas, Math.random()*30, Math.random()*30);
+            view.addChild(circle);
+            circles.push(circle);
+
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +47,14 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i])
+            } 
 
-            
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            for (var i = 0; i < circles.length; i++) {
+                game.checkCirclePosition(circles[i])
+            }
 
             // TODO 9 : Iterate over the array
            
@@ -62,8 +74,18 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            // if the circles goes past the left side of the screen it will be placed on the RIGHT
+            if (circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            // if circles go up out of screen they will appear on the BOTTOM
+            if (circle.y < 0) {
+                circle.y = canvas.height;
+            }
+            // if circles go down out of screen they will reappear at the top
+            if (circle.y > canvas.height) {
+                circle.y = 0
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
