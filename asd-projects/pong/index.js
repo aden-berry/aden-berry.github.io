cc940ae358226b9529a6e33f615ccfda15d01592
aden-berry <berry446@stcharles.k12.la.us>
2024-05-20 03:28:36 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,27 +10,49 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+
   // Game Item Objects
-var rightPaddle = {
-  
-};
-var leftPaddle = {
+  var rightPaddle = {
+    id: '#rightPaddle',
+    positionX: 700,
+    positionY: 0,
+    speedX: 0,
+    speedY: 0,
+    height: parseFloat($('#rightPaddle').height()),
+  };
+  var leftPaddle = {
+    id: '#leftPaddle',
+    positionX: 50,
+    positionY: 0,
+    speedX: 0,
+    speedY: 0,
+    height: parseFloat($('#leftPaddle').height()),
+  };
+  var ball = {
+    id: '#ball',
+    positionX: 0,
+    positionY: 0,
+    speedX: 4,
+    speedY: 4,
+    width: parseFloat($('#ball').width()), 
+    height: parseFloat($('#ball').height()),
+  };
+  var P1Score = 0;
+  var P2Score = 0;
+  var boardWidth = parseFloat($('#board').width());
+  var boardHeight = parseFloat($('#board').height());
 
-};
-var ball = {
-
-};
-var scoreP1 = {
-  id: $('#scoreP1')
-}
-var scoreP2 = {
-  id: $('#scoreP2')
-}
+  // Imaginary Number Declearation thingy //
+  var KEYS = {
+    W: 87,
+    S: 83,
+    UPARROW: 38,
+    DOWNARROW: 40,
+  }
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handlePaddles);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', stopHandlePaddles);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -40,22 +62,112 @@ var scoreP2 = {
   by calling this function and executing the code inside.
   */
   function newFrame() {
+    scoreKeeper(P1Score);
+    scoreKeeper(P2Score);
+    repositionThangs(leftPaddle);
+    repositionThangs(rightPaddle);
+    repositionThangs(ball);
+    ballBoundary();
+    boarderLimit(leftPaddle);
+    boarderLimit(rightPaddle);
+    redrawThangs(leftPaddle);
+    redrawThangs(rightPaddle);
+    redrawThangs(ball);
     
-
   }
-  
+
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  //makes paddles move//
+  function handlePaddles(event) {
+    if (event.which === KEYS.W) {
+      leftPaddle.speedY = -5;
+      console.log("w")
+    }
+    if (event.which === KEYS.S) {
+      leftPaddle.speedY = 5;
+      console.log("s")
+    }
+    if (event.which === KEYS.UPARROW) {
+      rightPaddle.speedY = -5
+    }
+    if (event.which === KEYS.DOWNARROW) {
+      rightPaddle.speedY = 5
+    }
+  }
+  //handles the handler//
+  function stopHandlePaddles(event) {
+    if (event.which !== KEYS.W) {
+      leftPaddle.speedY = 0;
+    }
+    if (event.which !== KEYS.S) {
+      leftPaddle.speedY = 0;
+    }
+    if (event.which !== KEYS.UPARROW) {
+      rightPaddle.speedY = 0;
+      console.log("up")
+    }
+    if (event.which !== KEYS.DOWNARROW) {
+      rightPaddle.speedY = 0;
+      console.log("down")
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  //supposed to keep score //
+  function scoreKeeper (side){
+    if (side === "left") {
+      P2Score++;
+    }
+    if (side === "right"){
+      P1Score++;
+    }
 
-  
+  }
+  //the balls limit and when it bounces //
+  function ballBoundary (){
+    // if (ball.positionX && ball.positionY >= ){
+      // 
+    // }
+    if (ball.positionY  <= 0){
+      ball.speedY *= -1;
+    }
+    if (ball.positionX  <= 0){
+      ball.speedX *= -1;
+      scoreKeeper("left");
+      console.log(P2Score);
+    }
+    if (ball.positionY + ball.height >= boardHeight + 20){
+      ball.speedY *= -1;
+    }
+    if (ball.positionX + ball.width >= boardWidth + 20){
+      ball.speedX *= -1;
+
+    }
+    
+  }
+  /// this is just for the paddles limit//
+  function boarderLimit(obj){
+    if (obj.positionY  <= 0){
+      obj.positionY = 0;
+    }
+    if (obj.positionY + obj.height >= boardHeight + 20){
+      obj.positionY = boardHeight - obj.height + 20;
+    }
+  }
+  // key reason the game items can move since it remakes them in said spot//
+  function redrawThangs(item) {
+    $(item.id).css("top", item.positionY);
+    $(item.id).css("left", item.positionX);
+  }
+  //similar to other function above but involves speed//
+  function repositionThangs(item) {
+    item.positionX += item.speedX;
+    item.positionY += item.speedY;
+  }
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -63,5 +175,5 @@ var scoreP2 = {
     // turn off event handlers
     $(document).off();
   }
-  
+
 }
